1969b2508a7101913c4b5e2d1bca0a56bed303e0
Arun <arunsrinivasan333@gmail.com>
2024-03-14 09:46:12 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import { useRef, useState } from 'react';
 
 function App() {
+const [first, setfirst] = useState("")
+const scsmsg=useRef();
+const errmsg=useRef();
+
+function Handle(){
+if(first=="admin" ){
+  scsmsg.current.style.display="block"
+}
+else if(first==""){
+  errmsg.current.style.display="block"
+}
+
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    <div>
+      <h1>Login</h1>
+      <br></br>
+      <label data-testid="lbl1">Enter your name:</label><br></br>
+      <input data-testid="txt1" type="text" value={first} onChange={(e)=>{setfirst(e.target.value)}} ></input><br></br>
+
+      <label data-testid="lbl2">Enter your password:</label><br></br>
+      <input data-testid="txt2" type="password"></input><br></br><br></br>
+
+      <button data-testid="btnSubmit" type="submit" onClick={Handle}>Submit</button>
+      <p data-testid="message1" className='msg' ref={scsmsg}>Login Successful!</p>
+      <p data-testid="message2" className='msg2' ref={errmsg}>Please fill all the details</p>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,60 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('My Test Suite',()=>{
+
+  test('Test case-1[Checking]', () => {
+    render(<App />);
+    const text = screen.getByText('Login');
+    expect(text).toBeInTheDocument();
+  });
+
+  test('Test Case-2[Rendex All Elements]',()=>{
+    render(<App/>)
+    const TextBox1=screen.getByTestId("txt1")
+    const TextBox2=screen.getByTestId("txt2")
+    const Label1=screen.getByTestId("lbl1")
+    const Label2=screen.getByTestId("lbl2")
+    const Button=screen.getByTestId("btnSubmit")
+
+    expect(TextBox1).toBeInTheDocument()
+    expect(TextBox2).toBeInTheDocument()
+    expect(Label1).toBeInTheDocument()
+    expect(Label2).toBeInTheDocument()
+    expect(Button).toBeInTheDocument()
+  });
+
+test('Test Case-3[Render Initial Value]',()=>{
+  render(<App/>)
+  // const TextBox1=screen.getByTestId("txt1")
+  // const TextBox2=screen.getByTestId("txt2")
+  const Button=screen.getByTestId("btnSubmit")
+  // const Message1=screen.getByTestId("message1")
+  
+  fireEvent.click(Button)
+
+  const Message2=screen.getByTestId("message2")
+
+  expect(Message2).toHaveTextContent("Please fill all the details")
+
 });
+
+test('Test Case-4[Testing Success Result]',()=>{
+  render(<App/>)
+  const TextBox1=screen.getByTestId("txt1")
+  const Message1=screen.getByTestId("message1")
+  const Button=screen.getByTestId("btnSubmit")
+
+  fireEvent.change(TextBox1 ,{target : {value :"a"}})
+  fireEvent.click(Button)
+
+  expect(Message1).toBeInTheDocument("Login Successful!")
+
+
+
+});
+
+
+});
+
+
